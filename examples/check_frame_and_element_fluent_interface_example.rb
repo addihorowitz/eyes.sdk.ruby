3fc69ffdb8e9f2a612b6c9606e209fec31e4161d
addihorowitz <addihorowitz@gmail.com>
2017-08-07 15:08:28 +0300

    travis config

--- examples/check_frame_and_element_fluent_interface_example.rb
+++ examples/check_frame_and_element_fluent_interface_example.rb
@@ -44,22 +44,4 @@ def run_test
   end
 end
 
-platforms = ['Windows 10', 'Linux', 'macOS 10.12']
-browsers = %w(chrome firefox)
-platforms.each do |platform|
-  browsers.each do |browser|
-    caps = {
-      platform: platform,
-      browserName: browser
-    }
-    SauceDriver.update_caps caps
-    # todo: add try catch (include all)
-    run_test
-  end
-end
-caps = {
-  platform:  'Windows 10',
-  browserName:  'internet explorer'
-}
-SauceDriver.update_caps caps
-run_test
+run_tests(run_test)
--- examples/common.rb
+++ examples/common.rb
@@ -1,23 +1,23 @@
 require_relative './sauce_driver'
 
 def run_tests(test)
-    platforms = ['Windows 10', 'Linux', 'macOS 10.12']
-    browsers = %w(chrome firefox)
-    platforms.each do |platform|
-      browsers.each do |browser|
-        caps = {
-          platform: platform,
-          browserName: browser
-        }
-        SauceDriver.update_caps caps
-        # todo: add try catch (include all)
-        run_test
-      end
+  platforms = ['Windows 10', 'Linux', 'macOS 10.12']
+  browsers = %w(chrome firefox)
+  platforms.each do |platform|
+    browsers.each do |browser|
+      caps = {
+        platform: platform,
+        browserName: browser
+      }
+      SauceDriver.update_caps caps
+      # TODO: add try catch (include all)
+      run_test
     end
-    caps = {
-      platform:  'Windows 10',
-      browserName:  'internet explorer'
-    }
-    SauceDriver.update_caps caps
-    test
+  end
+  caps = {
+    platform:  'Windows 10',
+    browserName:  'internet explorer'
+  }
+  SauceDriver.update_caps caps
+  test
 end
